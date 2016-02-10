421821e7e3325ef7c2dcd60b2c417b7cd1d64803
Naseem Sarwar <naseem@iiadigital.co.uk>
2016-02-10 14:31:16 +0000

    Clubs index page content changes.

--- resources/views/clubs/show.blade.php
+++ resources/views/clubs/show.blade.php
@@ -1,12 +1,11 @@
 @extends('app')
 
 @section('content')
-<h2>Projects</h2>
+<h2>Clubs</h2>
 
 @if ( !$club->count() )
-You have no projects
+You have no clubs
 @else
-
     <h1> {{$club->name}}</h1>
     <p>Lorem ipsum doler sit amit...</p>
 @endif
